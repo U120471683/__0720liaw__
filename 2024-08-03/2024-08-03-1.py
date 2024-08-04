13a8c7eb53fcc2047b998c57a45dff24ede05864
U120471683 <shichanliaw@gmail.com>
2024-08-05 06:14:38 +0800

    dfs

--- 2024-08-03/2024-08-03-1.py
+++ 2024-08-03/2024-08-03-1.py
@@ -5,8 +5,7 @@ def main():
     now = datetime.now()
     current_file_name = now.strftime('%Y_%m_%d.log')
     log_path = file.created_log_file(current_file_name)
-    tool.file.record_info(log_path)
-    
+    tools.file.record_info(log_path)
     
 if __name__ == '__main__':
-    main()
+    main()f
